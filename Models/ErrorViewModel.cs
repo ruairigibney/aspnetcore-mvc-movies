9069bee4ff87397403063e78c30fc0fa2b54f1fa
Scott McElveen <smcelveen@smashfly.com>
2019-01-18 10:39:59 +0000

    :sunrise: boilerplate dotnet new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnetcore_mvc_movies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
